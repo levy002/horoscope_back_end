dc91a1293e4f39e62fbd1c6c181ff042a06bac22
“levy-DELL” <levy.ukwi002@gmail.com>
2022-10-20 01:48:39 +0200

    Create login logic controller

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 const signup = async (req, res) => {
   const { name, email, password, birthdate } = req.body;
@@ -10,13 +11,49 @@ const signup = async (req, res) => {
         email: user.email,
         birthdate: user.birthdate,
       },
-      message: "Successfully signup",
+      message: "Signup successfully",
     });
   } catch (err) {
     console.log(err);
   }
 };
 
+const loginUserVerification = async (email, password) => {
+  const user = await User.findOne({email});
+
+  if(user) {
+     const passwordAuth = await bcrypt.compare(password, user.password);
+
+     if(passwordAuth){
+        return user;
+     }else {
+        throw Error('Invalid password');
+     }
+  }else {
+    throw Error('Invalid email');
+  }
+};
+
+const login = async(req, res) => {
+  const { email, password } = req.body;
+
+  try{
+     const user = await loginUserVerification( email, password);
+     console.log(user, 'logged in user');
+     res.status(200).send({
+       user: {
+         name: user.name,
+         email: user.email,
+         birthdate: user.birthdate,
+       },
+       message: "Login successfully",
+     });
+  }catch(err){
+   console.log(err);
+  }
+};
+
 module.exports = {
   signup,
+  login
 };
