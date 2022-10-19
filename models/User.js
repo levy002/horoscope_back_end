a91f43003375b33ec6ef1751bf2ca0dd7aee1c8d
“levy-DELL” <levy.ukwi002@gmail.com>
2022-10-20 00:37:32 +0200

    Hash password before save new user into database

--- models/User.js
+++ models/User.js
@@ -25,6 +25,12 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  console.log(this.password, salt);
+});
+
 const User = mongoose.model("user", userSchema);
 
 module.exports = User;
