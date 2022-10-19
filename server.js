22cf425575a92bcaf59b7a02cb65db12f0365823
“levy-DELL” <levy.ukwi002@gmail.com>
2022-10-20 00:40:59 +0200

    Create sign up route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -18,3 +19,5 @@ mongoose
     app.listen(PORT);
   })
   .catch((err) => console.log(err));
+
+  app.use(authRoutes);
